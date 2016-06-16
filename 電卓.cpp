0cef69f5d4235d48661a526304099fe38c8895b7
ToruOwO <toorumon@gmail.com>
2016-06-16 20:57:05 +0900

    Ver4
    小数点に対応できるように更新しました。
    (≧▽≦)

--- 電卓.cpp
+++ 電卓.cpp
@@ -11,47 +11,63 @@ string s;
 int digit(){
 	assert(isdigit(s[cur]));
 	int n = s[cur] - '0';
-	cur = cur + 1;
+	cur++;
 	return n;
 }
 
-//digit読み取りの繰り返し→digitの整数への変換
-int number(){
-	int n = digit();
-	while(cur < s.length() && isdigit(s[cur])) n = n*10 + digit();
+//digit読み取りの繰り返し→digitの数への変換
+double number(){
+	double n = digit();
+	while(cur < s.length() && (isdigit(s[cur]) || s[cur] == '.')){
+		if(isdigit(s[cur])) {
+			n = n*10 + digit();
+			cur++;
+		}
+		
+		if (s[cur] == '.') {
+			cur++;
+			double m = 0.1;
+			while(isdigit(s[cur])){
+				n += digit()*m;
+				m *= 0.1;
+			}
+		}
+	} 
 	return n;
 }
+
+double factor();
 
 //掛け算と割り算の実装
 //優先度は＋ーより高いので、先にtermとして計算しておきます
-int term(){
-	int a = factor(); //カッコがあれば、カッコの中身を先に計算する
+double term(){
+	double a = factor(); //カッコがあれば、カッコの中身を先に計算する
 	while(cur < s.length() && (s[cur] == '*' || s[cur] == '/')){
 		char sign = s[cur++];
-		int b = factor();
+		double b = factor();
 		if(sign == '*') a *= b;
 		else a /= b;
 	}
 	return a;
 }
 
-int expression();
+double expression();
 
-int factor(){
+double factor(){
 	if(s[cur] != '(') return number();
-	cur++; int n = expression(); //カッコの中は必ずexpressionである
+	cur++; double n = expression(); //カッコの中は必ずexpressionである
 	assert(s[cur] == ')');
 	cur += 1;
 	return n;
 }
 
 //足し算と引き算の実装
-int expression(){
-	int sum = term();　//掛け算と割り算（もしあれば）のreturn値を使う
+double expression(){
+	double sum = term(); //掛け算と割り算（もしあれば）のreturn値を使う
 	while(s[cur] == '+' || s[cur] == '-'){
 		char sign = s[cur];
 		cur += 1;
-		int b = term();
+		double b = term();
 		if(sign == '+') sum += b;
 		else sum -= b;
 	}
@@ -61,7 +77,7 @@ int expression(){
 
 int main(){
 	cin >> s;
-	int a = expression();
+	double a = expression();
 	cout << a << endl;
 	return 0;
 }
